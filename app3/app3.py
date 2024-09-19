f12a3ed4161a4eb9c5f754b3f185d3e82decd9f6
endrycofr <endrycofarelr@gmail.com>
2024-09-19 13:23:08 +0000

    first commit

---
+++ app3/app3.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app1 = Flask(__name__)
+
+@app1.route('/')
+def hello_world():
+    return 'Hi, this is App 3'
+
+if __name__ == '__main__':
+    app1.run(debug=True, host='0.0.0.0')
+
